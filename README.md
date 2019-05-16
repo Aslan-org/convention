90281433d68a0ecd814695380c26c180e97348e5
Fosso Arcel <kraulain@gmail.com>
2019-05-16 19:08:26 +0100

    deleted folders that where moved to separate repositories

--- README.md
+++
@@ -1,1 +0,0 @@
-#Simple Conference Web Application
--- backend/src/main/java/org/afecam/convention/data/Collections.java
+++
@@ -1,5 +0,0 @@
-package org.afecam.convention.data;
-
-public enum Collections {
-    User, Participant, Message, Notification, PassCode, Articles;
-}
